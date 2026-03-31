930d029ce039284ea58fa99ff1cfd4e1d7616be8
hyunhyesoo <sue990831@gmail.com>
2026-03-31 11:39:25 +0900

    5장 요청처리 메소드의 파라미터 유형

--- src/main/java/kr/ac/kopo/hhs/_026exam/controller/EX08Controller.java
+++ src/main/java/kr/ac/kopo/hhs/_026exam/controller/EX08Controller.java
@@ -3,21 +3,16 @@ package kr.ac.kopo.hhs._026exam.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class EX06Controller {
-//    @GetMapping("/exam06")
-//    public String requestMethod(Model model){
-//        model.addAttribute("title1", "Model 유형 연습1");
-//        model.addAttribute("title2", "문자열 값 또는 객체 참조 값 전달 가능");
-//        return "viewPage";
-//    }
-
-    @GetMapping("/exam06")
-    public String requestMethod(Model model){
-        model.addAttribute("title1", "Model 유형 연습1");
-        model.addAttribute("title2", "문자열 값 또는 객체 참조 값 전달 가능");
-        return "view06";
+public class EX08Controller {
+    @GetMapping("/exam08")
+    public ModelAndView requestMethod(Model model){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("title1", "ModelAndView 연습");
+        modelAndView.addObject("title2", "ModelAndView addObject와 setViewName을 사용");
+        modelAndView.setViewName("viewPage");
+        return modelAndView;
     }
 }
